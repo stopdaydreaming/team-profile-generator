ce4d5bc118b06779fb84abc061a217310fa36b77
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-12 21:44:14 -0500

    add inquirer prompt for each role

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const questions = require("./lib/questions");
+const {
+  questions,
+  questionsManager,
+  questionsEngineer,
+  questionsIntern
+} = require("./lib/questions");
 
 const inquirer = require("inquirer");
 const path = require("path");
@@ -11,7 +16,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -26,24 +30,6 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-const team = [];
-
-function init() {
-    inquirer.prompt(questions).then(response => {
-      console.log(response);
-      fs.writeFile("./output/team.html", render(team) , (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("team page created");
-        }
-      });
-      return;
-    });
-  }
-  
-  init();
-
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
@@ -53,3 +39,39 @@ function init() {
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const team = [];
+
+function buildTeam() {
+  inquirer.prompt(questions).then(response => {
+    if (response.role === "Manager") {
+      inquirer.prompt(questionsManager).then(answerM => {
+        team.push(
+          new Manager(answerM.name, answerM.id, answerM.email, answerM.office)
+        );
+        buildTeam();
+      });
+    } else if (response.role === "Engineer") {
+      inquirer.prompt(questionsEngineer).then(answerE => {
+        team.push(
+          new Engineer(answerE.name, answerE.id, answerE.email, answerE.github)
+        );
+        buildTeam();
+      });
+    } else if (response.role === "Intern") {
+      inquirer.prompt(questionsIntern).then(answerI => {
+        team.push(
+          new Intern(answerI.name, answerI.id, answerI.email, answerI.school)
+        );
+        buildTeam();
+      });
+    } else {
+      if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+      }
+      fs.writeFileSync(outputPath, render(team));
+    }
+  });
+}
+
+buildTeam();
